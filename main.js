f5467de9f4e6089d1032ffee374c13b46468c188
Nikita <nikinegi1998@gmail.com>
2021-03-26 17:35:09 +0530

    modified

--- main.js
+++ main.js
@@ -8,23 +8,31 @@ btn.onclick = (e)=>{
 
     const v= input.value;
     // console.log(v);
+
+    // check whether the input box is not empty
     if(v==""){
         alert("Items field empty!! Please try again...");
     }
     else{
-        
+
+    // adding list item
     const li = document.createElement('li');
     li.innerText=v;
-
     li.classList.add('p-3');
-    const new_div = document.createElement('div');
+    li.classList.add('font-style');
+    const hr = document.createElement('hr');
 
+    // creating a new div
+    const new_div = document.createElement('div');    
     new_div.classList.add('float-end');
+
+    // delete button 
     const del_button = document.createElement('i');
     del_button.classList.add('fas');
     del_button.classList.add('fa-trash-alt');
     del_button.classList.add('del-color');
     
+    // edit button
     const edit_button = document.createElement('i');
     edit_button.classList.add('fas');
     edit_button.classList.add('fa-pencil-alt');
@@ -33,16 +41,18 @@ btn.onclick = (e)=>{
     
     ol_list.append(li);
     li.append(new_div);
+    li.append(hr);
     new_div.append(edit_button);
     new_div.append(del_button);
     input.value ="";
 
-
+    // delete button functionality
     del_button.onclick = (e)=>{
         // console.log(li);
         li.remove();
     }
 
+    // edit button functionality
     edit_button.onclick = (e)=>{
         input.value = li.innerText;
         const tmp = input.value;
